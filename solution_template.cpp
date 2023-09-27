4e35e12612dcd477725dc60039428e5c79d7f1cb
Combobyte <varun.sreedhar1@gmail.com>
2023-09-27 09:34:04 -0700

    Added basic solution template

---
+++ solution_template.cpp
@@ -0,0 +1,22 @@
+//Quick template for leetcode solutions
+
+int main(){
+    int num_wrong = 0;
+    
+    /*
+    // Test case 1
+    if(solution() != test1) ++num_wrong;
+    // Test case 2
+    if(solution() != test2) ++num_wrong;
+    // Test case 3
+    if(solution() != test3) ++num_wrong;
+    */
+    
+    return num_wrong;
+}
+
+/*
+___ solution(){
+
+}
+*/
