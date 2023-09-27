74905dcb3617907a87409b72b3e0568797f6f330
Combobyte <varun.sreedhar1@gmail.com>
2023-09-27 09:23:52 -0700

    Made run script more robust

--- run.sh
+++ run.sh
@@ -1,12 +1,29 @@
-#! /bin/sh
+#!/bin/sh
 
-if [ ! -d "$1" ]; then
-    echo "No solution directory named $1"
+files=(*)
+solution_directory=""
+
+if [ "$1" == "" ]; then
+    echo "Usage: Pass in the problem # you want to build and run (ex: ./run 32)"
+    exit 1
+fi
+
+for((i=0; i<${#files[@]}; i++)); do
+    if [ ${files[$i]} == "$1" ]; then
+        solution_directory=${files[$i]}
+    elif [[ ${files[$i]} =~ $1-.* ]]; then
+        solution_directory=${files[$i]}
+    fi
+done
+
+if [ ! -d "$solution_directory" ]; then
+    echo "No solution directory named or prefixed by $1"
 else
-    echo "Building solution for $1..."
-    echo "Running solution for $1..."
+    cd "$solution_directory"
+    echo "Building solution for $solution_directory..."
+    echo "Running solution for $solution_directory..."
     error=0
-    if [ $error -eq 0]; then
+    if [ ! $error -eq 0 ]; then
 	echo "Error in test case $error"
     else
         echo "Test cases passed"
